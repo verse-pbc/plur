247d9bd4661b5e5e35bd69d97f3ace6d52846aef
Martin Dutra <martindsq@hotmail.com>
2025-02-12 18:08:31 -0300

    Add docs

--- lib/router/index/index_drawer_content.dart
+++ lib/router/index/index_drawer_content.dart
@@ -16,7 +16,9 @@ import '../../util/table_mode_util.dart';
 import 'account_manager_widget.dart';
 import '../../data/join_group_parameters.dart';
 
+/// A drawer widget that displays user information and navigation options.
 class IndexDrawerContent extends StatefulWidget {
+  /// Determines if the drawer should be in compact mode.
   final bool smallMode;
 
   const IndexDrawerContent({super.key, required this.smallMode});
@@ -25,12 +27,17 @@ class IndexDrawerContent extends StatefulWidget {
   State<StatefulWidget> createState() => _IndexDrawerContentState();
 }
 
+/// The state class for [IndexDrawerContent].
 class _IndexDrawerContentState extends State<IndexDrawerContent> {
-  ScrollController userStatisticscontroller = ScrollController();
+  /// Width of the profile edit button.
+  ///
+  /// Defaults to 40.
+  final double _profileEditBtnWidth = 40;
 
-  double profileEditBtnWidth = 40;
-
-  bool readOnly = false;
+  /// Determines if the drawer is in read-only mode.
+  ///
+  /// Defaults to false.
+  bool _readOnly = false;
 
   @override
   Widget build(BuildContext context) {
@@ -44,8 +51,9 @@ class _IndexDrawerContentState extends State<IndexDrawerContent> {
     var cardColor = themeData.cardColor;
     var hintColor = themeData.hintColor;
     List<Widget> list = [];
-    readOnly = nostr!.isReadOnly();
+    _readOnly = nostr!.isReadOnly();
 
+    // Add user profile picture or metadata display based on smallMode
     if (widget.smallMode) {
       list.add(Container(
         margin: EdgeInsets.only(
@@ -77,19 +85,20 @@ class _IndexDrawerContentState extends State<IndexDrawerContent> {
         Positioned(
           top: paddingTop + Base.BASE_PADDING_HALF,
           right: Base.BASE_PADDING,
-          child: readOnly
+          child: _readOnly
               ? Container()
               : Container(
-                  height: profileEditBtnWidth,
-                  width: profileEditBtnWidth,
+                  height: _profileEditBtnWidth,
+                  width: _profileEditBtnWidth,
                   decoration: BoxDecoration(
                     color: cardColor,
-                    borderRadius:
-                        BorderRadius.circular(profileEditBtnWidth / 2),
+                    borderRadius: BorderRadius.circular(
+                      _profileEditBtnWidth / 2,
+                    ),
                   ),
                   child: IconButton(
                     icon: const Icon(Icons.edit_square),
-                    onPressed: jumpToProfileEdit,
+                    onPressed: _jumpToProfileEdit,
                   ),
                 ),
         ),
@@ -97,6 +106,8 @@ class _IndexDrawerContentState extends State<IndexDrawerContent> {
     }
 
     List<Widget> centerList = [];
+
+    // Add the HOME option to the list of drawer items.
     if (TableModeUtil.isTableMode()) {
       centerList.add(IndexDrawerItemWidget(
         iconData: Icons.home_rounded,
@@ -112,6 +123,7 @@ class _IndexDrawerContentState extends State<IndexDrawerContent> {
       ));
     }
 
+    // Add the SETTINGS option to the list of drawer items.
     centerList.add(IndexDrawerItemWidget(
       iconData: Icons.settings_rounded,
       name: localization.Settings,
@@ -121,6 +133,7 @@ class _IndexDrawerContentState extends State<IndexDrawerContent> {
       smallMode: widget.smallMode,
     ));
 
+    // Add the ADD_TEST_GROUPS option to the list of drawer items.
     centerList.add(IndexDrawerItemWidget(
       iconData: Icons.group_add,
       name: 'Add test groups',
@@ -138,6 +151,7 @@ class _IndexDrawerContentState extends State<IndexDrawerContent> {
       smallMode: widget.smallMode,
     ));
 
+    // Add a flexible space to send the Account Manager widget to the bottom.
     list.add(Expanded(
       child: SingleChildScrollView(
         child: Column(
@@ -147,6 +161,7 @@ class _IndexDrawerContentState extends State<IndexDrawerContent> {
       ),
     ));
 
+    // Add the Account Manager widget.
     list.add(IndexDrawerItemWidget(
       iconData: Icons.account_box_rounded,
       name: localization.Account_Manager,
@@ -157,36 +172,37 @@ class _IndexDrawerContentState extends State<IndexDrawerContent> {
     ));
 
     if (widget.smallMode) {
+      // Add a button to exit small mode.
       list.add(Container(
         margin: const EdgeInsets.only(bottom: Base.BASE_PADDING_HALF),
         child: IndexDrawerItemWidget(
           iconData: Icons.last_page_rounded,
           name: "",
-          onTap: toggleSmallMode,
+          onTap: _toggleSmallMode,
           smallMode: widget.smallMode,
         ),
       ));
     } else {
+      // Add the app version.
       Widget versionWidget = Text("V ${Base.VERSION_NAME}");
-
       if (TableModeUtil.isTableMode()) {
+        // Add a button to enter small mode.
         List<Widget> subList = [];
         subList.add(GestureDetector(
-          onTap: toggleSmallMode,
+          onTap: _toggleSmallMode,
           behavior: HitTestBehavior.translucent,
           child: Container(
             margin: const EdgeInsets.only(right: Base.BASE_PADDING),
             child: const Icon(Icons.first_page_rounded),
           ),
         ));
+        // Place the app version at the right side.
         subList.add(versionWidget);
-
         versionWidget = Row(
           crossAxisAlignment: CrossAxisAlignment.start,
           children: subList,
         );
       }
-
       list.add(Container(
         margin: const EdgeInsets.only(top: Base.BASE_PADDING_HALF),
         padding: const EdgeInsets.only(
@@ -195,11 +211,8 @@ class _IndexDrawerContentState extends State<IndexDrawerContent> {
           top: Base.BASE_PADDING,
         ),
         decoration: BoxDecoration(
-            border: Border(
-                top: BorderSide(
-          width: 1,
-          color: hintColor,
-        ))),
+          border: Border(top: BorderSide(width: 1, color: hintColor)),
+        ),
         alignment: Alignment.centerLeft,
         child: versionWidget,
       ));
@@ -215,11 +228,13 @@ class _IndexDrawerContentState extends State<IndexDrawerContent> {
     );
   }
 
-  void jumpToProfileEdit() {
+  /// Navigates to the profile edit screen.
+  void _jumpToProfileEdit() {
     var metadata = metadataProvider.getMetadata(nostr!.publicKey);
     RouterUtil.router(context, RouterPath.PROFILE_EDITOR, metadata);
   }
 
+  /// Displays the account manager modal bottom sheet.
   void _showBasicModalBottomSheet(context) async {
     showModalBottomSheet(
       isScrollControlled: false,
@@ -230,7 +245,8 @@ class _IndexDrawerContentState extends State<IndexDrawerContent> {
     );
   }
 
-  toggleSmallMode() {
+  /// Toggles between compact and expanded drawer modes.
+  void _toggleSmallMode() {
     var callback = IndexPcDrawerWrapperCallback.of(context);
     if (callback != null) {
       callback.toggle();
@@ -238,21 +254,33 @@ class _IndexDrawerContentState extends State<IndexDrawerContent> {
   }
 }
 
+/// A widget representing an item inside the navigation drawer.
 class IndexDrawerItemWidget extends StatelessWidget {
+  /// The icon to be displayed in the item.
   final IconData iconData;
 
+  /// The label text for the item.
   final String name;
 
+  /// Callback function when the item is tapped.
   final Function onTap;
 
+  /// Optional callback function when the item is double-tapped.
   final Function? onDoubleTap;
 
+  /// Optional callback function when the item is long-pressed.
   final Function? onLongPress;
 
+  /// Optional color for the icon and text.
   final Color? color;
 
+  /// Indicates if the widget is being displayed in a compact mode.
   final bool smallMode;
 
+  /// Creates an instance of [IndexDrawerItemWidget].
+  ///
+  /// The [iconData], [name], and [onTap] parameters are required.
+  /// The [smallMode] parameter defaults to `false`.
   const IndexDrawerItemWidget({
     super.key,
     required this.iconData,
@@ -266,6 +294,7 @@ class IndexDrawerItemWidget extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    // The icon widget
     Widget iconWidget = Icon(
       iconData,
       color: color,
@@ -273,6 +302,7 @@ class IndexDrawerItemWidget extends StatelessWidget {
 
     Widget mainWidget;
     if (smallMode) {
+      // Compact mode: Only the icon is displayed with minimal padding.
       mainWidget = Container(
         decoration: BoxDecoration(
           color: color != null ? Colors.white.withOpacity(0.1) : null,
@@ -283,20 +313,20 @@ class IndexDrawerItemWidget extends StatelessWidget {
         child: iconWidget,
       );
     } else {
-      List<Widget> list = [];
-      list.add(Container(
-        margin: const EdgeInsets.only(
-          left: Base.BASE_PADDING * 2,
-          right: Base.BASE_PADDING,
-        ),
-        child: iconWidget,
-      ));
-      list.add(Text(name, style: TextStyle(color: color)));
-
+      // Normal mode: Display icon alongside text.
       mainWidget = SizedBox(
         height: 34,
         child: Row(
-          children: list,
+          children: [
+            Container(
+              margin: const EdgeInsets.only(
+                left: Base.BASE_PADDING * 2,
+                right: Base.BASE_PADDING,
+              ),
+              child: iconWidget,
+            ),
+            Text(name, style: TextStyle(color: color)),
+          ],
         ),
       );
     }
