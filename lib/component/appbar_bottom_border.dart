6e02112f728eff059a070abb934b216747df13d6
Itunu Raimi <chavitunu@gmail.com>
2025-02-17 17:26:38 +0100

    add doc to appbar bottom border widget

--- lib/component/appbar_bottom_border.dart
+++ lib/component/appbar_bottom_border.dart
@@ -1,6 +1,19 @@
 import 'package:flutter/material.dart';
 import '../util/theme_util.dart';
 
+/// A widget that adds a bottom border to an AppBar.
+///
+/// It creates a 1-pixel border that automatically uses
+/// the theme's separator color. It's designed to be used in the AppBar's
+/// bottom property.
+///
+/// Example:
+/// ```dart
+/// AppBar(
+///   bottom: AppBarBottomBorder(),
+///   title: Text('App Title'),
+/// )
+/// ```
 class AppBarBottomBorder extends StatelessWidget
     implements PreferredSizeWidget {
   const AppBarBottomBorder({super.key});
@@ -21,6 +34,7 @@ class AppBarBottomBorder extends StatelessWidget
     );
   }
 
+  /// Defines a constant height of 1.0 pixels for the border.
   @override
   Size get preferredSize => const Size.fromHeight(1.0);
 }
