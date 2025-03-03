82cf6d52b7da46c78aa577aee7040c9ee9c04dbd
mplorentz <mplorentz@users.noreply.github.com>
2025-03-03 12:00:53 -0500

    Pull GroupIdentifier host out of event.sources

--- test/nostr_sdk/event_relation_test.dart
+++ test/nostr_sdk/event_relation_test.dart
@@ -5,7 +5,22 @@ import '../helpers/test_data.dart';
 
 void main() {
   group('EventRelation', () {
-    test('correctly parses group ID from h tag', () {
+    test('correctly parses group identifier from h tag', () {
+      // Arrange
+      final eventJson = TestData.groupNoteJson;
+      const relay = "wss://communities.nos.social";
+
+      // Act
+      final event = Event.fromJson(eventJson);
+      event.sources = [relay];
+      final relation = EventRelation.fromEvent(event);
+
+      // Assert
+      expect(relation.groupIdentifier?.groupId, equals("SW8N7TKHLDVZ"));
+      expect(relation.groupIdentifier?.host, equals(relay));
+    });
+
+    test("groupIdentifier fails to parse when we can't find relay", () {
       // Arrange
       final eventJson = TestData.groupNoteJson;
 
@@ -14,8 +29,7 @@ void main() {
       final relation = EventRelation.fromEvent(event);
 
       // Assert
-      expect(relation.groupIdentifier?.groupId, equals("SW8N7TKHLDVZ"));
-      expect(relation.groupIdentifier?.host, equals(""));
+      expect(relation.groupIdentifier, isNull);
     });
   });
 } 
