54d7434885e261dc5ad3a90c0a620bee50e72c0a
mplorentz <mplorentz@users.noreply.github.com>
2025-03-03 18:05:07 -0500

    Fix super.key being on the same line as initializer.

--- lib/router/web_utils/web_util_item_widget.dart
+++ lib/router/web_utils/web_util_item_widget.dart
@@ -8,7 +8,11 @@ class WebUtilItemWidget extends StatelessWidget {
 
   String des;
 
-  WebUtilItemWidget({super.key, required this.link, required this.des});
+  WebUtilItemWidget({
+    super.key,
+    required this.link,
+    required this.des,
+  });
 
   @override
   Widget build(BuildContext context) {
