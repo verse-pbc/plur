22d71d2cdd0ac27eb21ae9bda5997e8f12f19f7f
mplorentz <mplorentz@users.noreply.github.com>
2025-03-03 18:05:07 -0500

    Fix super.key being on the same line as initializer.

--- lib/component/appbar4stack.dart
+++ lib/component/appbar4stack.dart
@@ -14,7 +14,8 @@ class Appbar4Stack extends StatefulWidget {
 
   Widget? action;
 
-  Appbar4Stack({super.key, 
+  Appbar4Stack({
+    super.key, 
     this.title,
     this.textColor,
     this.backgroundColor,
