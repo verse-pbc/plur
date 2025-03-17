233ae6db18f1dd0a0be729b3417d02db2da606db
Itunu Raimi <chavitunu@gmail.com>
2025-03-17 16:26:21 +0100

    add localised Admin string

--- lib/component/group/admin_tag_widget.dart
+++ lib/component/group/admin_tag_widget.dart
@@ -1,12 +1,13 @@
 import 'package:flutter/material.dart';
 import 'package:nostrmo/util/theme_util.dart';
-
+import 'package:nostrmo/generated/l10n.dart';
 class AdminTagWidget extends StatelessWidget {
   const AdminTagWidget({super.key});
 
   @override
   Widget build(BuildContext context) {
     final themeData = Theme.of(context);
+    final localization = S.of(context);
 
     return Container(
       padding: const EdgeInsets.symmetric(horizontal: 10, vertical: 4),
@@ -15,7 +16,7 @@ class AdminTagWidget extends StatelessWidget {
         borderRadius: BorderRadius.circular(50),
       ),
       child: Text(
-        'Admin',
+        localization.Admin,
         style: TextStyle(
           fontSize: 12,
           color: themeData.customColors.dimmedColor,
