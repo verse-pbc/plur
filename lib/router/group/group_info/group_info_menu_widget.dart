6a30d7e97bb20efe5ec14a90ff7791b641574bc7
Itunu Raimi <chavitunu@gmail.com>
2025-03-17 16:20:30 +0100

    add group info components

---
+++ lib/router/group/group_info/group_info_menu_widget.dart
@@ -0,0 +1,65 @@
+import 'package:flutter/material.dart';
+import 'package:nostrmo/util/theme_util.dart';
+import 'package:nostrmo/util/router_util.dart';
+import 'package:nostrmo/consts/router_path.dart';
+import 'package:nostrmo/nostr_sdk/nostr_sdk.dart';
+import 'package:nostrmo/router/group/group_info/group_info_menu_item_widget.dart';
+
+enum GroupInfoMenuItem {
+  members('Members');
+
+  final String title;
+  const GroupInfoMenuItem(this.title);
+}
+
+class GroupInfoMenuWidget extends StatelessWidget {
+  final GroupIdentifier groupId;
+
+  const GroupInfoMenuWidget({
+    super.key,
+    required this.groupId,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    final themeData = Theme.of(context);
+
+    return Material(
+      color: themeData.customColors.feedBgColor,
+      clipBehavior: Clip.antiAlias,
+      borderRadius: BorderRadius.circular(10),
+      child: ListView.builder(
+          shrinkWrap: true,
+          padding: EdgeInsets.zero,
+          physics: const NeverScrollableScrollPhysics(),
+          itemCount: _menuItems.length,
+          itemBuilder: (context, index) {
+            final item = _menuItems[index];
+            return Column(
+              mainAxisSize: MainAxisSize.min,
+              children: [
+                GroupInfoMenuItemWidget(
+                  title: item.title,
+                  onTap: () => _navigateToMenuItem(context, item),
+                ),
+                if (index < _menuItems.length - 1)
+                  Divider(
+                    height: 1,
+                    color: themeData.customColors.navBgColor,
+                  ),
+              ],
+            );
+          },
+        ),
+    );
+  }
+
+  void _navigateToMenuItem(BuildContext context, GroupInfoMenuItem item) {
+    switch (item) {
+      case GroupInfoMenuItem.members:
+        RouterUtil.router(context, RouterPath.GROUP_MEMBERS, groupId);
+    }
+  }
+
+  List<GroupInfoMenuItem> get _menuItems => GroupInfoMenuItem.values;
+}
