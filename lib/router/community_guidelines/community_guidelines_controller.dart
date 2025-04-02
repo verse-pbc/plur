55d11e04c5e813b2c742b6703476e84a6f561cd8
Martin Dutra <martindsq@hotmail.com>
2025-04-02 12:00:17 -0300

    create a new folder

--- lib/router/community_guidelines/community_guidelines_controller.dart
+++ lib/router/community_guidelines/community_guidelines_controller.dart
@@ -6,7 +6,7 @@ import 'package:nostr_sdk/nostr_sdk.dart';
 import '../../data/group_metadata_repository.dart';
 
 class CommunityGuidelinesController
-    extends FamilyAsyncNotifier<String, GroupIdentifier> {
+    extends AutoDisposeFamilyAsyncNotifier<String, GroupIdentifier> {
   Future<String> _fetchCommunityGuidelines(GroupIdentifier id) async {
     final repository = ref.watch(groupMetadataRepositoryProvider);
     final groupMetadata = await repository.fetchGroupMetadata(id);
@@ -18,9 +18,6 @@ class CommunityGuidelinesController
     return _fetchCommunityGuidelines(arg);
   }
 
-  Future<void> save() async {
-    state = const AsyncValue<String>.loading();
-  }
 }
 
 final communityGuidelinesControllerProvider = AsyncNotifierProvider.autoDispose
