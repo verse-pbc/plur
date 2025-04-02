793380d471afca486d4c73f6a7573510982ee143
Martin Dutra <martindsq@hotmail.com>
2025-04-02 14:42:21 -0300

    Document CommunityGuidelinesController

--- lib/router/community_guidelines/community_guidelines_controller.dart
+++ lib/router/community_guidelines/community_guidelines_controller.dart
@@ -6,8 +6,20 @@ import 'package:nostr_sdk/nostr_sdk.dart';
 
 import '../../data/group_metadata_repository.dart';
 
+/// A controller class that manages the community guidelines for a group.
 class CommunityGuidelinesController
     extends AutoDisposeFamilyAsyncNotifier<String, GroupIdentifier> {
+  /// Fetches the community guidelines for a given group identifier.
+  ///
+  /// This function retrieves the group metadata from the repository and
+  /// returns the community guidelines. If no guidelines are found, an empty
+  /// string is returned.
+  ///
+  /// - Parameters:
+  ///   - id: The identifier of the group for which community guidelines are
+  ///   to be fetched.
+  /// - Returns: A `Future` that resolves to the community guidelines of the
+  /// specified group.
   Future<String> _fetchCommunityGuidelines(GroupIdentifier id) async {
     final repository = ref.watch(groupMetadataRepositoryProvider);
     final groupMetadata = await repository.fetchGroupMetadata(id);
@@ -19,6 +31,17 @@ class CommunityGuidelinesController
     return _fetchCommunityGuidelines(arg);
   }
 
+  /// Saves the community guidelines for a group.
+  ///
+  /// This function updates the community guidelines of the specified group
+  /// in the repository. If the new guidelines are the same as the current
+  /// ones, the function returns `true` immediately. Otherwise, it updates
+  /// the state and saves the new guidelines.
+  ///
+  /// - Parameters:
+  ///   - communityGuidelines: The new community guidelines to be saved.
+  /// - Returns: A `Future` that resolves to `true` if the guidelines were
+  /// successfully saved, otherwise `false`.
   Future<bool> save(String communityGuidelines) async {
     GroupIdentifier id = arg;
     if (state.hasValue && state.value == communityGuidelines) {
@@ -42,6 +65,8 @@ class CommunityGuidelinesController
   }
 }
 
+/// A provider that supplies an instance of `CommunityGuidelinesController`
+/// for a given group identifier.
 final communityGuidelinesControllerProvider = AsyncNotifierProvider.autoDispose
     .family<CommunityGuidelinesController, String, GroupIdentifier>(
         CommunityGuidelinesController.new);
