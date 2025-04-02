20e2d47f0319f7e72cf6a6710a7b803fae42d6c6
Martin Dutra <martindsq@hotmail.com>
2025-04-02 14:05:27 -0300

    Save changes in relays

--- lib/router/community_guidelines/community_guidelines_screen.dart
+++ lib/router/community_guidelines/community_guidelines_screen.dart
@@ -9,14 +9,17 @@ import '../../util/router_util.dart';
 import '../../util/theme_util.dart';
 import 'community_guidelines_controller.dart';
 
-class CommunityGuidelinesScreen extends StatefulWidget {
+class CommunityGuidelinesScreen extends ConsumerStatefulWidget {
   const CommunityGuidelinesScreen({super.key});
 
   @override
-  State<StatefulWidget> createState() => _CommunityGuidelinesScreenState();
+  ConsumerState<ConsumerStatefulWidget> createState() {
+    return _CommunityGuidelinesScreenState();
+  }
 }
 
-class _CommunityGuidelinesScreenState extends State<CommunityGuidelinesScreen> {
+class _CommunityGuidelinesScreenState
+    extends ConsumerState<CommunityGuidelinesScreen> {
   final _descriptionController = TextEditingController();
 
   @override
@@ -30,13 +33,13 @@ class _CommunityGuidelinesScreenState extends State<CommunityGuidelinesScreen> {
     final borderSide = BorderSide(color: secondaryForegroundColor);
     var bodyLargeFontSize = themeData.textTheme.bodyLarge!.fontSize;
     final localization = S.of(context);
-
     var arg = RouterUtil.routerArgs(context);
     if (arg == null || arg is! GroupIdentifier) {
       RouterUtil.back(context);
       return Container();
     }
     final id = arg;
+    final controller = ref.watch(communityGuidelinesControllerProvider(id));
     return Scaffold(
       appBar: AppBar(
         leading: const AppbarBackBtnWidget(),
@@ -49,53 +52,67 @@ class _CommunityGuidelinesScreenState extends State<CommunityGuidelinesScreen> {
             fontWeight: FontWeight.bold,
           ),
         ),
-      ),
-      body: Consumer(builder: (context, ref, _) {
-        final controller = ref.watch(communityGuidelinesControllerProvider(id));
-        return controller.when(
-          data: (value) {
-            _descriptionController.text = value;
-            return SingleChildScrollView(
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  const SizedBox(height: 26),
-                  Padding(
-                    padding: const EdgeInsets.symmetric(horizontal: 16),
-                    child: TextFormField(
-                      controller: _descriptionController,
-                      decoration: InputDecoration(
-                        labelText: localization.Description,
-                        hintText: localization.Enter_Community_Description,
-                        alignLabelWithHint: true,
-                        border: OutlineInputBorder(
-                          borderRadius: borderRadius,
-                          borderSide: borderSide,
-                        ),
-                        enabledBorder: OutlineInputBorder(
-                          borderRadius: borderRadius,
-                          borderSide: borderSide,
-                        ),
-                        focusedBorder: OutlineInputBorder(
-                          borderRadius: borderRadius,
-                          borderSide: borderSide,
-                        ),
-                        labelStyle: TextStyle(color: primaryForegroundColor),
-                      ),
-                      maxLines: null,
-                      minLines: 5,
-                      keyboardType: TextInputType.multiline,
-                      textAlignVertical: TextAlignVertical.top,
-                    ),
-                  ),
-                ],
+        actions: [
+          TextButton(
+            onPressed: () {
+              ref
+                  .read(communityGuidelinesControllerProvider(id).notifier)
+                  .save(_descriptionController.text);
+            },
+            style: const ButtonStyle(),
+            child: Text(
+              localization.Save,
+              style: TextStyle(
+                color: themeData.customColors.accentColor,
+                fontSize: 18,
               ),
-            );
-          },
-          error: (error, stackTrace) => Center(child: ErrorWidget(error)),
-          loading: () => const Center(child: CircularProgressIndicator()),
-        );
-      }),
+            ),
+          ),
+        ],
+      ),
+      body: controller.when(
+        data: (value) {
+          _descriptionController.text = value;
+          return SingleChildScrollView(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                const SizedBox(height: 26),
+                Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 16),
+                  child: TextFormField(
+                    controller: _descriptionController,
+                    decoration: InputDecoration(
+                      labelText: localization.Description,
+                      hintText: localization.Enter_Community_Description,
+                      alignLabelWithHint: true,
+                      border: OutlineInputBorder(
+                        borderRadius: borderRadius,
+                        borderSide: borderSide,
+                      ),
+                      enabledBorder: OutlineInputBorder(
+                        borderRadius: borderRadius,
+                        borderSide: borderSide,
+                      ),
+                      focusedBorder: OutlineInputBorder(
+                        borderRadius: borderRadius,
+                        borderSide: borderSide,
+                      ),
+                      labelStyle: TextStyle(color: primaryForegroundColor),
+                    ),
+                    maxLines: null,
+                    minLines: 5,
+                    keyboardType: TextInputType.multiline,
+                    textAlignVertical: TextAlignVertical.top,
+                  ),
+                ),
+              ],
+            ),
+          );
+        },
+        error: (error, stackTrace) => Center(child: ErrorWidget(error)),
+        loading: () => const Center(child: CircularProgressIndicator()),
+      ),
     );
   }
 }
