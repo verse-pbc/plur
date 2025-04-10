513492440f669d3800b79bf0cda31358e5630b7d
Itunu Raimi <chavitunu@gmail.com>
2025-04-10 20:42:10 +0100

    update onboarding template file

--- lib/router/onboarding/onboarding_step_widget.dart
+++ lib/router/onboarding/onboarding_step_widget.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import '../../component/primary_button_widget.dart';
+import '../../component/input_field_widget.dart';
 import '../../util/theme_util.dart';
 
 /// Represents a button in the onboarding step.
@@ -7,10 +8,12 @@ import '../../util/theme_util.dart';
 ///
 class OnboardingStepButton {
   final String text;
+  final bool enabled;
   final VoidCallback onTap;
 
   OnboardingStepButton({
     required this.text,
+    this.enabled = true,
     required this.onTap,
   });
 }
@@ -22,7 +25,9 @@ class OnboardingStepWidget extends StatelessWidget {
   final String emoji;
   final String title;
   final Key? titleKey;
-  final String description;
+  final String? description;
+  final TextEditingController? textController;
+  final String? textFieldHint;
   final List<OnboardingStepButton> buttons;
 
   const OnboardingStepWidget({
@@ -30,9 +35,15 @@ class OnboardingStepWidget extends StatelessWidget {
     required this.emoji,
     required this.title,
     this.titleKey,
-    required this.description,
+    this.description,
+    this.textController,
+    this.textFieldHint,
     required this.buttons,
-  });
+  }) : assert(
+          (description != null && textController == null) ||
+              (description == null && textController != null),
+          'Either description or textController must be provided, but not both',
+        );
 
   @override
   Widget build(BuildContext context) {
@@ -40,65 +51,74 @@ class OnboardingStepWidget extends StatelessWidget {
     final primaryForegroundColor =
         themeData.customColors.primaryForegroundColor;
 
-    return Padding(
-      padding: const EdgeInsets.all(62),
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          // Emoji
-          Text(
-            emoji,
-            textAlign: TextAlign.start,
-            style: const TextStyle(
-              fontSize: 64,
-              height: 1.2,
+    return MediaQuery(
+      data: MediaQuery.of(context).copyWith(viewInsets: EdgeInsets.zero),
+      child: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 62, vertical: 32),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            // Emoji
+            Text(
+              emoji,
+              textAlign: TextAlign.start,
+              style: const TextStyle(
+                fontSize: 64,
+                height: 1.2,
+              ),
             ),
-          ),
 
-          const SizedBox(height: 32),
+            const SizedBox(height: 32),
 
-          Text(
-            title,
-            key: titleKey,
-            textAlign: TextAlign.start,
-            style: TextStyle(
-              color: primaryForegroundColor,
-              fontSize: 32,
-              fontWeight: FontWeight.bold,
-              height: 1.2,
+            Text(
+              title,
+              key: titleKey,
+              textAlign: TextAlign.start,
+              style: TextStyle(
+                color: primaryForegroundColor,
+                fontSize: 32,
+                fontWeight: FontWeight.bold,
+                height: 1.2,
+              ),
             ),
-          ),
 
-          const SizedBox(height: 52),
+            const SizedBox(height: 52),
 
-          Text(
-            description,
-            textAlign: TextAlign.start,
-            style: TextStyle(
-              color: themeData.customColors.secondaryForegroundColor,
-              fontSize: 17,
-              height: 1.4,
-            ),
-          ),
-
-          const Spacer(),
-
-          Row(
-            children: List.generate(
-              buttons.length,
-              (index) => [
-                if (index > 0) const SizedBox(width: 22),
-                Expanded(
-                  child: PrimaryButtonWidget(
-                    text: buttons[index].text,
-                    borderRadius: 4,
-                    onTap: buttons[index].onTap,
+            description != null
+                ? Text(
+                    description!,
+                    textAlign: TextAlign.start,
+                    style: TextStyle(
+                      color: themeData.customColors.secondaryForegroundColor,
+                      fontSize: 17,
+                      height: 1.4,
+                    ),
+                  )
+                : InputFieldWidget(
+                    controller: textController!,
+                    hintText: textFieldHint,
                   ),
-                ),
-              ],
-            ).expand((widgets) => widgets).toList(),
-          )
-        ],
+
+            const Spacer(),
+
+            Row(
+              children: List.generate(
+                buttons.length,
+                (index) => [
+                  if (index > 0) const SizedBox(width: 22),
+                  Expanded(
+                    child: PrimaryButtonWidget(
+                      text: buttons[index].text,
+                      borderRadius: 4,
+                      onTap: buttons[index].onTap,
+                      enabled: buttons[index].enabled,
+                    ),
+                  ),
+                ],
+              ).expand((widgets) => widgets).toList(),
+            )
+          ],
+        ),
       ),
     );
   }
