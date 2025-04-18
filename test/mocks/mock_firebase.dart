fc821d433784e8fdbe7e49c14b94962fee9e58b8
mplorentz <mplorentz@users.noreply.github.com>
2025-04-18 11:19:06 -0400

    Fix lint errors

--- test/mocks/mock_firebase.dart
+++ test/mocks/mock_firebase.dart
@@ -1,4 +1,3 @@
-import 'package:firebase_core/firebase_core.dart';
 import 'package:firebase_core_platform_interface/firebase_core_platform_interface.dart';
 import 'package:flutter_test/flutter_test.dart';
 
