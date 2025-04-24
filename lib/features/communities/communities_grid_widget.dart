4621f8e497e205a4af00fda8f3a6990ea43e570c
Itunu Raimi <chavitunu@gmail.com>
2025-04-24 13:57:27 +0100

    add prominent create button to group list

--- lib/features/communities/communities_grid_widget.dart
+++ lib/features/communities/communities_grid_widget.dart
@@ -3,6 +3,9 @@ import 'package:nostr_sdk/nostr_sdk.dart';
 
 import '../../consts/router_path.dart';
 import '../../util/router_util.dart';
+import '../../features/create_community/create_community_dialog.dart';
+import '../../generated/l10n.dart';
+import '../../util/theme_util.dart';
 import 'community_widget.dart';
 
 /// A widget that displays a grid of communities.
@@ -14,6 +17,10 @@ class CommunitiesGridWidget extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final themeData = Theme.of(context);
+    final localization = S.of(context);
+    final separatorColor = themeData.customColors.separatorColor;
+
     return GridView.builder(
       padding: const EdgeInsets.symmetric(vertical: 52),
       gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
@@ -22,13 +29,56 @@ class CommunitiesGridWidget extends StatelessWidget {
         mainAxisSpacing: 32.0,
         childAspectRatio: 1,
       ),
-      itemCount: groupIds.length,
+      itemCount: groupIds.length + 1, // Add 1 for the create button
       itemBuilder: (context, index) {
-        final groupIdentifier = groupIds[index];
+        if (index == 0) {
+          // Create Community button
+          return InkWell(
+            onTap: () => CreateCommunityDialog.show(context),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Container(
+                  width: 120,
+                  height: 120,
+                  decoration: BoxDecoration(
+                    shape: BoxShape.circle,
+                    border: Border.all(
+                      color: separatorColor,
+                      width: 4,
+                    ),
+                  ),
+                  child: Center(
+                    child: Icon(
+                      Icons.add,
+                      size: 48,
+                      color: themeData.customColors.primaryForegroundColor,
+                    ),
+                  ),
+                ),
+                const SizedBox(height: 12),
+                SizedBox(
+                  width: 120,
+                  child: Text(
+                    localization.Create_Community,
+                    style: TextStyle(
+                      fontWeight: FontWeight.bold,
+                      fontSize: 20,
+                      color: themeData.textTheme.bodyMedium!.color,
+                    ),
+                    overflow: TextOverflow.ellipsis,
+                    textAlign: TextAlign.center,
+                    maxLines: 2,
+                  ),
+                ),
+              ],
+            ),
+          );
+        }
+        final groupIdentifier = groupIds[index - 1];
         return InkWell(
           onTap: () {
-            RouterUtil.router(
-                context, RouterPath.groupDetail, groupIdentifier);
+            RouterUtil.router(context, RouterPath.groupDetail, groupIdentifier);
           },
           child: CommunityWidget(groupIdentifier),
         );
